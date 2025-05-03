0b33601936ee3e63646e550922a670c5f6df22ab
shoaib memon <shoaibattari83@gmail.com>
2025-05-03 23:47:17 +0500

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1 className="bg-red-500 text-8xl text-white font-bold border-8 border-yellow-300 p-8">
+       Grave Yard Management System - SAAS
+      </h1>
+    </main>
+  );
+}
+
+export default App;

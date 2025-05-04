117fc245f2374d88a7a27c435e74ebeb3b9c3360
shoaib memon <shoaibattari83@gmail.com>
2025-05-04 10:26:42 +0500

    sub categories page add dynamic

--- src/components/Guest/SubCategory.jsx
+++ src/components/Guest/SubCategory.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import UserViewHero from "./UserViewHero";
 
 const SubCategory = () => {
-  return (
-    <div>SubCategory</div>
-  )
-}
+  const { subCategory } = useParams();
+  return <UserViewHero section={subCategory} />;
+};
 
-export default SubCategory
+export default SubCategory;
